998bc85051370d0d3a95765f31b93c20b61126a0
rifaak <rifaakhtarcrescent@gmail.com>
2026-09-08 09:55:55 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,17 @@
+import os , urllib.parse , urllib.request , render_template
+
+Gemini_api_key = "Gemini_API_Key";
+
+def home():
+  return render_template (" index.html")
+
+def create_app():
+
+  app = Flask(_name_)
+  app.register_blueprint(youtube_bp, url_prefix="/youtube")
+
+@app.route("/html")
+def html():
+  return render_template("index.html")
+return app;
 
