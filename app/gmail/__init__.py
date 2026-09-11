ce1de042ebcd18556468fea2295dd4205bda0135
rifaak <rifaakhtarcrescent@gmail.com>
2026-09-11 09:32:33 +0530

    Update __init__.py

--- app/gmail/__init__.py
+++ app/gmail/__init__.py
@@ -1,2 +1,2 @@
-from .gmail_wrire import is_email_command, extract_email, create_gmail_url
-from.gmail_gen import generate_email_with_gemini
+from .gmail_write import is_email_command, extract_email, create_gmail_url
+from .gmail_gen import generate_email_with_gemini
